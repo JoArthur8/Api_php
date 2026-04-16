7ec04a5834910357604732d2fd58305c7f7ca612
JoArthur8 <joaoarthur.ripoli>
2026-04-16 10:24:56 -0300

    16042026

--- aula06-09042026/api.php
+++ aula06-09042026/api.php
@@ -3,15 +3,40 @@
 
     $metodo = $_SERVER['REQUEST_METHOD'];
     // echo "Método de requisição: " . $metodo;
+    $arquivo = "usuarios.json";
+
+    if(!file_exists($arquivo)){
+        file_put_contents($arquivo, json_encode([], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    }
+
+    $usuarios = json_decode(file_get_contents($arquivo),true);
+    // [
+    //     ["id" => 1, "nome" =>"Maria Souza", "email" => "maria@email.com"],
+    //     ["id" => 2, "nome" =>"João Silva", "email" => "joao@email.com"]
+    // ];
+
+
 
     switch ($metodo) {
         case 'GET':
-            
-            echo "Aqui ações de método GET";
+
+            // echo "Aqui ações de método GET";
+            echo json_encode($usuarios);
             break;
         
         case 'POST':
-            echo "Aqui ações de método POST";
+            // echo "Aqui ações de método POST";
+            $dados = json_decode(file_get_contents('php://input'), true);
+            // print_r($dados);
+            $novoUsuario = [
+                "id" => $dados["id"],
+                "nome" => $dados["nome"],
+                "email" => $dados["email"]
+            ];
+            array_push($usuarios,$novoUsuario);
+            // file_put_contents($arquivo,$usuarios);
+            echo json_encode('Usuario inserido');
+            print_r($usuarios);
             break;
         
         default:
@@ -19,10 +44,5 @@
             break;
     }
 
-    // $usuarios = [
-    //     ["id" => 1, "nome" =>"Maria Souza", "email" => "maria@email.com"],
-    //     ["id" => 2, "nome" =>"João Silva", "email" => "joao@email.com"]
-    // ];
-
-    // echo json_encode($usuarios);
+    
 ?>
